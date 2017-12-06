af29838a366f59ba5ace7554bbb96f1bf84f32d8
Razi Khaja <razi.khaja@gmail.com>
2017-12-06 14:53:59 -0500

    Coded several new methods.

--- src/cassandra_jinja2/logback_xml.py
+++ src/cassandra_jinja2/logback_xml.py
@@ -25,9 +25,17 @@ class LogbackXml(cassandra_jinja2.base_config.BaseConfig):
         self.systemlog_logdir()
         self.systemlog_rolling_logdir()
         self.systemlog_maxindex()
+        self.systemlog_maxfilesize()
         self.debuglog_logdir()
         self.debuglog_rolling_logdir()
         self.debuglog_maxindex()
+        self.debuglog_maxfilesize()
+        self.root_log_level()
+        self.appender_systemlog_enabled()
+        self.appender_stdout_enabled()
+        self.appender_asyncdebuglog_enabled()
+        self.cassandra_logger_level()
+        self.thrift_logger_level()
 
     def systemlog_logdir(self):
         """
@@ -50,20 +58,38 @@ class LogbackXml(cassandra_jinja2.base_config.BaseConfig):
     def systemlog_maxindex(self):
         """
         In this method, the option_pattern specified is:
-            r'^(\s*<appender name="SYSTEMLOG"(?:.*\n)+?\s*<maxIndex>)(\d+)(</maxIndex>)\n'
+            r'^(\s*<appender name="SYSTEMLOG"(?:.*\n)+?\s*<maxIndex>)(\w+)(</maxIndex>)\n'
         A portion of the regular expression above denoted by (?:.*\n)+?\s* requires some explanation. First, (?:...)
         denotes a non-capturing version of regular parentheses. Thus, after matching "SYSTEMLOG", we match any number of
-        characters, up and including a newline, one or more times, in a non-greedy fashion.  Then, preceding <maxIndex>
-        we match any number of whitespace characters.  It is important to match in a non-greedy fashion, which matches 
-        as little text as possible because if we didn't, the regex would match other text up until the last <maxIndex> 
+        characters, up to and including a newline, one or more times, in a non-greedy fashion.  Then, preceding <maxIndex>
+        we match any number of whitespace characters.  It is important to match in a non-greedy fashion, which matches
+        as little text as possible because if we didn't, the regex would match other text up until the last <maxIndex>
         tag later in the logback.xml file which would not be the desired result.
 
         <appender name="SYSTEMLOG" ... <maxIndex>20</maxIndex>
         :return:
         """
         self.add_jinja_for_option_with_default_value(
-            option_pattern=r'^(\s*<appender name="SYSTEMLOG"(?:.*\n)+?\s*<maxIndex>)(\d+)(</maxIndex>)\n',
+            option_pattern=r'^(\s*<appender name="SYSTEMLOG"(?:.*\n)+?\s*<maxIndex>)(\w+)(</maxIndex>)\n',
             jinja_variable='logback_xml.systemlog_maxindex')
+
+    def systemlog_maxfilesize(self):
+        """
+        In this method, the option_pattern specified is:
+            r'^(\s*<appender name="SYSTEMLOG"(?:.*\n)+?\s*<maxFileSize>)(\w+)(</maxFileSize>)\n'
+        A portion of the regular expression above denoted by (?:.*\n)+?\s* requires some explanation. First, (?:...)
+        denotes a non-capturing version of regular parentheses. Thus, after matching "SYSTEMLOG", we match any number of
+        characters, up to and including a newline, one or more times, in a non-greedy fashion.  Then, preceding <maxFileSize>
+        we match any number of whitespace characters.  It is important to match in a non-greedy fashion, which matches
+        as little text as possible because if we didn't, the regex would match other text up until the last <maxFileSize>
+        tag later in the logback.xml file which would not be the desired result.
+
+        <appender name="SYSTEMLOG" ... <maxFileSize>20</maxFileSize>
+        :return:
+        """
+        self.add_jinja_for_option_with_default_value(
+            option_pattern=r'^(\s*<appender name="SYSTEMLOG"(?:.*\n)+?\s*<maxFileSize>)(\w+)(</maxFileSize>)\n',
+            jinja_variable='logback_xml.systemlog_maxfilesize')
 
     def debuglog_logdir(self):
         """
@@ -86,17 +112,90 @@ class LogbackXml(cassandra_jinja2.base_config.BaseConfig):
     def debuglog_maxindex(self):
         """
         In this method, the option_pattern specified is:
-            r'^(\s*<appender name="DEBUGLOG"(?:.*\n)+?\s*<maxIndex>)(\d+)(</maxIndex>)\n'
+            r'^(\s*<appender name="DEBUGLOG"(?:.*\n)+?\s*<maxIndex>)(\w+)(</maxIndex>)\n'
         A portion of the regular expression above denoted by (?:.*\n)+?\s* requires some explanation. First, (?:...)
         denotes a non-capturing version of regular parentheses. Thus, after matching "DEBUGLOG", we match any number of
-        characters, up and including a newline, one or more times, in a non-greedy fashion.  Then, preceding <maxIndex>
-        we match any number of whitespace characters.  It is important to match in a non-greedy fashion, which matches 
-        as little text as possible because if we didn't, the regex would match other text up until the last <maxIndex> 
+        characters, up to and including a newline, one or more times, in a non-greedy fashion.  Then, preceding <maxIndex>
+        we match any number of whitespace characters.  It is important to match in a non-greedy fashion, which matches
+        as little text as possible because if we didn't, the regex would match other text up until the last <maxIndex>
         tag later in the logback.xml file which would not be the desired result.
 
         <appender name="DEBUGLOG" ... <maxIndex>20</maxIndex>
         :return:
         """
         self.add_jinja_for_option_with_default_value(
-            option_pattern=r'^(\s*<appender name="DEBUGLOG"(?:.*\n)+?\s*<maxIndex>)(\d+)(</maxIndex>)\n',
+            option_pattern=r'^(\s*<appender name="DEBUGLOG"(?:.*\n)+?\s*<maxIndex>)(\w+)(</maxIndex>)\n',
             jinja_variable='logback_xml.debuglog_maxindex')
+
+    def debuglog_maxfilesize(self):
+        """
+        In this method, the option_pattern specified is:
+            r'^(\s*<appender name="DEBUGLOG"(?:.*\n)+?\s*<maxFileSize>)(\w+)(</maxFileSize>)\n'
+        A portion of the regular expression above denoted by (?:.*\n)+?\s* requires some explanation. First, (?:...)
+        denotes a non-capturing version of regular parentheses. Thus, after matching "DEBUGLOG", we match any number of
+        characters, up to and including a newline, one or more times, in a non-greedy fashion.  Then, preceding <maxFileSize>
+        we match any number of whitespace characters.  It is important to match in a non-greedy fashion, which matches
+        as little text as possible because if we didn't, the regex would match other text up until the last <maxFileSize>
+        tag later in the logback.xml file which would not be the desired result.
+
+        <appender name="DEBUGLOG" ... <maxFileSize>20</maxFileSize>
+        :return:
+        """
+        self.add_jinja_for_option_with_default_value(
+            option_pattern=r'^(\s*<appender name="DEBUGLOG"(?:.*\n)+?\s*<maxFileSize>)(\w+)(</maxFileSize>)\n',
+            jinja_variable='logback_xml.debuglog_maxfilesize')
+
+    def root_log_level(self):
+        """
+        <root level="INFO">
+        :return:
+        """
+        self.add_jinja_for_option_with_default_value(
+            option_pattern=r'^(\s*<root level=")(.*)(">)\n',
+            jinja_variable='logback_xml.root_log_level')
+
+    def appender_systemlog_enabled(self):
+        """
+        <appender-ref ref="SYSTEMLOG" />
+        :return:
+        """
+        print('something')
+        self.add_jinja_to_comment_xml_option(
+            option_pattern=r'^(\s*)(<appender-ref ref="SYSTEMLOG" />)\n',
+            jinja_variable='logback_xml.appender_systemlog_enabled')
+
+    def appender_stdout_enabled(self):
+        """
+        <appender-ref ref="STDOUT" />
+        :return:
+        """
+        self.add_jinja_to_comment_xml_option(
+            option_pattern=r'^(\s*)(<appender-ref ref="STDOUT" />)\n',
+            jinja_variable='logback_xml.appender_stdout_enabled')
+
+    def appender_asyncdebuglog_enabled(self):
+        """
+        <appender-ref ref="ASYNCDEBUGLOG" /> <!-- Comment this line to disable debug.log -->
+        :return:
+        """
+        self.add_jinja_to_comment_xml_option(
+            option_pattern=r'^(\s*)(<appender-ref ref="ASYNCDEBUGLOG" />)(.*)\n',
+            jinja_variable='logback_xml.appender_asyncdebuglog_enabled')
+
+    def cassandra_logger_level(self):
+        """
+        <logger name="org.apache.cassandra" level="DEBUG"/>
+        :return:
+        """
+        self.add_jinja_for_option_with_default_value(
+            option_pattern=r'^(\s*<logger name="org.apache.cassandra" level=")(.*)("/>)\n',
+            jinja_variable='logback_xml.cassandra_logger_level')
+
+    def thrift_logger_level(self):
+        """
+        <logger name="com.thinkaurelius.thrift" level="ERROR"/>
+        :return:
+        """
+        self.add_jinja_for_option_with_default_value(
+            option_pattern=r'^(\s*<logger name="com.thinkaurelius.thrift" level=")(.*)("/>)\n',
+            jinja_variable='logback_xml.cassandra_logger_level')
